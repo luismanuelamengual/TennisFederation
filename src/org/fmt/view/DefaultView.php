5f46ecfedeeb4d34264bdb94d0c2f144ae4e31b1
Luis <luis.amengual@sitrack.com>
2016-01-26 12:59:08 -0300

    Cambios en la defaultView del fmt

--- src/org/fmt/view/DefaultView.php
+++ src/org/fmt/view/DefaultView.php
@@ -2,51 +2,61 @@
 
 namespace org\fmt\view;
 
+use com\bootstrap\component\BSNavBar;
 use com\bootstrap\view\BSPage;
+use NeoPHP\web\html\HTMLPage;
+use NeoPHP\web\html\HTMLTag;
 
 abstract class DefaultView extends BSPage
 {
+    private $mainNavBar;
+    private $mainContent;
+    
     public function __construct()
     {
         parent::__construct();
         $this->setTitle($this->getApplication()->getName());
         $this->addStyleFile($this->getBaseUrl() . "res/css/site.css");
-        $this->getBodyTag()->add($this->createMainHeader());
-        $this->getBodyTag()->add($this->createMainContent());
+        $this->mainNavBar = $this->createMainNavBar();
+        $this->mainContent = $this->createMainContent();
+        $this->addElement($this->mainNavBar);
+        $this->addElement($this->mainContent);
     }
     
-    protected function createMainHeader ()
+    private function createMainNavBar ()
     {
-        return '
-        <div id="mainNavbar" class="navbar navbar-default navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a href="#" class="navbar-brand">' . $this->getApplication()->getName() . '</a>
-                </div>
-                 <div class="navbar-collapse collapse navbar-responsive-collapse">
-                    ' . $this->createHeaderContent() . '
-                </div>
-            </div>
-        </div>';
+        $mainHeader = new BSNavBar();
+        $mainHeader->setId("mainnavbar");
+        $mainHeader->setBrand($this->getApplication()->getName());
+        $mainHeader->setStyle(BSNavBar::STYLE_DEFAULT);
+        return $mainHeader;
     }
     
-    protected function createMainContent ()
+    private function createMainContent ()
     {
-        return '
-        <div id="mainContent">
-            <div id="mainBody">
-                ' . $this->createContent() . '
-            </div>
-        </div>';
+        $mainContent = new class extends \com\bootstrap\component\BSComponent
+        {
+            public function build (HTMLPage $page, HTMLTag $parent)
+            {
+                $mainBodyDiv = new HTMLTag("div", ["id"=>"mainbody"]);
+                parent::build($page, $mainBodyDiv);
+                $mainDiv = new HTMLTag("div", ["id"=>"maincontainer"]);
+                $mainDiv->add($mainBodyDiv);
+                $parent->add($mainDiv);
+            }
+        };
+        return $mainContent;
     }
     
-    protected abstract function createHeaderContent();
-    protected abstract function createContent();
+    function getMainNavBar() 
+    {
+        return $this->mainNavBar;
+    }
+
+    function getMainContent() 
+    {
+        return $this->mainContent;
+    }
 }
 
 ?>
--- src/org/fmt/view/PortalView.php
+++ src/org/fmt/view/PortalView.php
@@ -4,91 +4,94 @@ namespace org\fmt\view;
 
 class PortalView extends DefaultView
 {
-    protected function build()
+    public function __construct()
     {
-        parent::build();
-        $this->getBodyTag()->add($this->createMessageBox());
-        $this->addScript('
-            function checkPageLocation ()
-            {
-                var src = window.location.href;
-                if(window.top != window.self) {
-                    window.open(src,"_top");
-                }
-            }
-
-            function clearErrorMessage ()
-            {
-                $("#messageBox .modal-body").html("");
-            }
-
-            function showErrorMessage (message)
-            {
-                $("#messageBox .modal-body").html(message);
-                $("#messageBox").modal("show");
-            }
-
-            function disableLoginControls ()
-            {
-                $("input[name=username]").prop("disabled", true);
-                $("input[name=password]").prop("disabled", true);
-                $("button[name=loginbutton]").prop("disabled", true);
-                $("body").css("cursor", "progress");
-            }
-
-            function enableLoginControls ()
-            {
-                $("input[name=username]").prop("disabled", false);
-                $("input[name=password]").prop("disabled", false);
-                $("button[name=loginbutton]").prop("disabled", false);
-                $("body").css("cursor", "default");
-                $("input[name=username]").focus();
-            }
-
-            $("button[name=loginbutton]").click(function(event)
-            {
-                clearErrorMessage();
-                disableLoginControls();
-                var username = $("input[name=username]")[0].value;
-                var password = $("input[name=password]")[0].value;
-                $.ajax("' . $this->getUrl("session/") . '?username=" + username + "&password=" + password,
-                {
-                    method: "PUT",
-                    success: function (response)
-                    {
-                        var data = jQuery.parseJSON(response);
-                        if (data.success)
-                        {
-                            window.open("' . $this->getUrl("site/") . '", "_self");
-                        }
-                        else
-                        {
-                            showErrorMessage(data.errorMessage);
-                            enableLoginControls();
-                        }
-                    },
-                    error: function (qXHR, textStatus, errorThrown)
-                    {
-                        if (qXHR.responseText)
-                        {
-                            var responseObject = jQuery.parseJSON(qXHR.responseText);
-                            showErrorMessage(responseObject.errorMessage);
-                        }
-                        else
-                        {
-                            showErrorMessage(textStatus + " - " + errorThrown);
-                        }
-                        enableLoginControls();
-                    },
-                    timeout: function ()
-                    {
-                        showErrorMessage("Se ha agotado el tiempo de conexión. Intente más tarde");
-                        enableLoginControls();
-                    }
-                });
-                return false;
-            });
-        ');
+        parent::__construct();
+//        $this->getMainHeader()->addElement("hola");
+        
+        
+//        $this->getBodyTag()->add($this->createMessageBox());
+//        $this->addScript('
+//            function checkPageLocation ()
+//            {
+//                var src = window.location.href;
+//                if(window.top != window.self) {
+//                    window.open(src,"_top");
+//                }
+//            }
+//
+//            function clearErrorMessage ()
+//            {
+//                $("#messageBox .modal-body").html("");
+//            }
+//
+//            function showErrorMessage (message)
+//            {
+//                $("#messageBox .modal-body").html(message);
+//                $("#messageBox").modal("show");
+//            }
+//
+//            function disableLoginControls ()
+//            {
+//                $("input[name=username]").prop("disabled", true);
+//                $("input[name=password]").prop("disabled", true);
+//                $("button[name=loginbutton]").prop("disabled", true);
+//                $("body").css("cursor", "progress");
+//            }
+//
+//            function enableLoginControls ()
+//            {
+//                $("input[name=username]").prop("disabled", false);
+//                $("input[name=password]").prop("disabled", false);
+//                $("button[name=loginbutton]").prop("disabled", false);
+//                $("body").css("cursor", "default");
+//                $("input[name=username]").focus();
+//            }
+//
+//            $("button[name=loginbutton]").click(function(event)
+//            {
+//                clearErrorMessage();
+//                disableLoginControls();
+//                var username = $("input[name=username]")[0].value;
+//                var password = $("input[name=password]")[0].value;
+//                $.ajax("' . $this->getUrl("session/") . '?username=" + username + "&password=" + password,
+//                {
+//                    method: "PUT",
+//                    success: function (response)
+//                    {
+//                        var data = jQuery.parseJSON(response);
+//                        if (data.success)
+//                        {
+//                            window.open("' . $this->getUrl("site/") . '", "_self");
+//                        }
+//                        else
+//                        {
+//                            showErrorMessage(data.errorMessage);
+//                            enableLoginControls();
+//                        }
+//                    },
+//                    error: function (qXHR, textStatus, errorThrown)
+//                    {
+//                        if (qXHR.responseText)
+//                        {
+//                            var responseObject = jQuery.parseJSON(qXHR.responseText);
+//                            showErrorMessage(responseObject.errorMessage);
+//                        }
+//                        else
+//                        {
+//                            showErrorMessage(textStatus + " - " + errorThrown);
+//                        }
+//                        enableLoginControls();
+//                    },
+//                    timeout: function ()
+//                    {
+//                        showErrorMessage("Se ha agotado el tiempo de conexión. Intente más tarde");
+//                        enableLoginControls();
+//                    }
+//                });
+//                return false;
+//            });
+//        ');
     }
     
     protected function createMessageBox()
