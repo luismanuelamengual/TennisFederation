fe8183e92f5e9523a734e402ffb672c710272138
Luis <luis.amengual@sitrack.com>
2016-02-25 16:23:25 -0300

    Correcciones en el portal

--- resources/templates/base.blade.php
+++ resources/templates/base.blade.php
@@ -8,7 +8,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="{{ $this->getResourceUrl('css/site.css') }}" />
-        @yield("stylesheets")
+        @yield("stylesheetFiles")
     </head>
     <body>
         
@@ -71,22 +71,21 @@
                     
                     @endif
                     
+                    @if (!isset($this->getSession()->sessionId))
+                    
+                    <form class="navbar-form navbar-right">
+                        <div class="form-group">
+                            <input type="text" id="usernameField" placeholder="Usuario" class="form-control" autofocus>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" id="passwordField" placeholder="Contraseña" class="form-control">
+                        </div>
+                        <button id="loginButton" type="submit" class="btn btn-primary">Ingresar</button>
+                    </form>
+                    
+                    @else
+                    
                     <ul class="nav navbar-nav navbar-right">
-                    
-                        @if (!isset($this->getSession()->sessionId))
-                        
-                        <form class="navbar-form">
-                            <div class="form-group">
-                                <input type="text" id="usernameField" placeholder="Usuario" class="form-control" autofocus>
-                            </div>
-                            <div class="form-group">
-                                <input type="password" id="passwordField" placeholder="Contraseña" class="form-control">
-                            </div>
-                            <button id="loginButton" type="submit" class="btn btn-primary">Ingresar</button>
-                        </form>
-                        
-                        @else
-                    
                         <li class="nav-item dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ $this->getSession()->firstname . " " . $this->getSession()->lastname }} <span class="caret"></span></a>
                             <ul class="dropdown-menu">
@@ -98,10 +97,10 @@
                                 </li>
                             </ul>
                         </li>
-                        
-                        @endif  
-                        
                     </ul>
+                    
+                    @endif
+                    
                 </div>
             </div>
         </nav>
