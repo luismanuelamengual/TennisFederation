2acbe1130dfc91ab2563bc818281efe4ad4e7841
Luis <luis.amengual@sitrack.com>
2016-03-10 09:24:20 -0300

    Obtencion de rutas a partir de las properties

--- public/index.php
+++ public/index.php
@@ -4,8 +4,6 @@ require_once ("../../NeoPHP3/autoload.php");
 $app = new NeoPHP\web\WebApplication(realpath(".."));
 $app->setName ("Federación Mendocina de Tenis");
 $app->setRestfull (true);
-$app->addRoute("/", "org\\fmt\\controller\\SiteController");
-$app->addRoute("/session/", "org\\fmt\\controller\\SessionController");
 $app->handleRequest();
 
 ?>
