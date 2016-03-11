6d3d83201dbd8e44d59eba40cb55c2e819e636b5
Luis <luis.amengual@sitrack.com>
2016-03-11 09:56:35 -0300

    Correcciones menores

--- resources/templates/public/base.blade.php
+++ resources/templates/public/base.blade.php
@@ -1,14 +1,14 @@
 @extends ("base")
 
-@section("login")
-    <form class="navbar-form navbar-right">
+@section("mainbarContents")
+    <form class="form-inline pull-xs-right">
         <div class="form-group">
             <input type="text" id="usernameField" placeholder="Usuario" class="form-control" autofocus>
         </div>
         <div class="form-group">
             <input type="password" id="passwordField" placeholder="Contraseña" class="form-control">
         </div>
-        <button id="loginButton" type="submit" class="btn btn-primary">Ingresar</button>
+        <button id="loginButton" type="button" class="btn btn-danger">Ingresar</button>
     </form>
 @stop
 
