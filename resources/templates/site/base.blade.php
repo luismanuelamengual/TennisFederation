ec3e0c29f16d6967c8acf70da487b0b31b083a8c
Luis <luis.amengual@sitrack.com>
2016-03-14 15:57:43 -0300

    cambios menores

--- resources/templates/site/base.blade.php
+++ resources/templates/site/base.blade.php
@@ -25,23 +25,23 @@
                     <div class="profile-usermenu">
                         <ul class="nav">
                             
-                            <li class="active"><a href="#"><i class="glyphicon glyphicon-home"></i>Inicio</a></li>
+                            <li class="active"><a href="{{ $this->getUrl("/dashboard/") }}"><i class="glyphicon glyphicon-home"></i>Inicio</a></li>
                             
                             @if ($this->getSession()->type == UserType::USERTYPE_ADMINISTRATOR)
-                            <li><a href="#"><i class="glyphicon glyphicon-triangle-right"></i>Adm Usuarios</a></li>
-                            <li><a href="#"><i class="glyphicon glyphicon-triangle-right"></i>Adm Categorías</a></li>
-                            <li><a href="#"><i class="glyphicon glyphicon-triangle-right"></i>Adm Clubes</a></li>
+                            <li><a href="{{ $this->getUrl("/user/") }}"><i class="glyphicon glyphicon-triangle-right"></i>Adm Usuarios</a></li>
+                            <li><a href="{{ $this->getUrl("/category/") }}"><i class="glyphicon glyphicon-triangle-right"></i>Adm Categorías</a></li>
+                            <li><a href="{{ $this->getUrl("/club/") }}"><i class="glyphicon glyphicon-triangle-right"></i>Adm Clubes</a></li>
                             @endif
                             
                             @if ($this->getSession()->type == UserType::USERTYPE_ADMINISTRATOR or $this->getSession()->type == UserType::USERTYPE_ORGANIZER)
-                            <li><a href="#"><i class="glyphicon glyphicon-triangle-right"></i>Adm Torneos</a></li>
-                            <li><a href="#"><i class="glyphicon glyphicon-triangle-right"></i>Adm Rankings</a></li>
-                            <li><a href="#"><i class="glyphicon glyphicon-triangle-right"></i>Adm Anuncios</a></li>
+                            <li><a href="{{ $this->getUrl("/tournament/") }}"><i class="glyphicon glyphicon-triangle-right"></i>Adm Torneos</a></li>
+                            <li><a href="{{ $this->getUrl("/ranking/") }}"><i class="glyphicon glyphicon-triangle-right"></i>Adm Rankings</a></li>
+                            <li><a href="{{ $this->getUrl("/announcement/") }}"><i class="glyphicon glyphicon-triangle-right"></i>Adm Anuncios</a></li>
                             @endif
                             
                             @if ($this->getSession()->type == UserType::USERTYPE_ADMINISTRATOR or $this->getSession()->type == UserType::USERTYPE_ORGANIZER or $this->getSession()->type == UserType::USERTYPE_PLAYER)
-                            <li><a href="#"><i class="glyphicon glyphicon-triangle-right"></i>Ver Torneos</a></li>
-                            <li><a href="#"><i class="glyphicon glyphicon-triangle-right"></i>Ver Rankings</a></li>
+                            <li><a href="{{ $this->getUrl("/tournament/showTournaments") }}"><i class="glyphicon glyphicon-triangle-right"></i>Ver Torneos</a></li>
+                            <li><a href="{{ $this->getUrl("/ranking/showRankings") }}"><i class="glyphicon glyphicon-triangle-right"></i>Ver Rankings</a></li>
                             @endif
                             
                         </ul>
