e3bed16a0b595578cd1f6f0ed4a2e47e19c88f27
Luis Manuel Amengual <luismanuelamengual@gmail.com>
2016-04-01 23:37:37 -0300

    Simplificación de los manejadores de modelos

--- resources/templates/site/categories.blade.php
+++ resources/templates/site/categories.blade.php
@@ -1,32 +1,33 @@
 @extends ("site.base")
 
 @section ("mainContents")
-    <div class="panel panel-default">
-        <div class="panel-heading">Categorias</div>
-        <div class="panel-body">
-            <ul class="nav nav-pills">
-                <li><button id="createButton" class="btn btn-default"><span class="glyphicon glyphicon-file"></span> Crear</button></li>
-                <li><button id="updateButton" class="btn btn-default" disabled="disabled"><span class="glyphicon glyphicon-pencil"></span> Modifiar</button></li>
-                <li><button id="deleteButton" class="btn btn-danger" disabled="disabled"><span class="glyphicon glyphicon-trash"></span> Eliminar</button></li>
-            </ul>
-            <table id="crudTable" class="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Descripción</th>
-                        <th>Tipo de Partido</th>
-                    <tr>
-                </thead>
-                <tbody>
-                    @foreach ($categories as $category)
-                    <tr data-categoryid="{{ $category->getId() }}">
-                        <td>{{ $category->getId() }}</td>
-                        <td>{{ $category->getDescription() }}</td>
-                        <td>{{ $category->getMatchType() == 1? "Singles":"Dobles" }}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h2>Administración de categorías</h2>
+            </div>
+
+            <div class="card-body table-responsive">
+                <table id="crudTable" class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Descripción</th>
+                            <th>Tipo de Partido</th>
+                        <tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($categories as $category)
+                        <tr data-categoryid="{{ $category->getId() }}">
+                            <td>{{ $category->getId() }}</td>
+                            <td>{{ $category->getDescription() }}</td>
+                            <td>{{ $category->getMatchType() == 1? "Singles":"Dobles" }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @stop 
