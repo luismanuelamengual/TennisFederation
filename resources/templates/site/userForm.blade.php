3b4cef5158d5bf026f0b36b59cc01ebd8145d426
Luis Manuel Amengual <luismanuelamengual@gmail.com>
2016-04-03 18:25:12 -0300

    cambios en vistas

--- resources/templates/site/userForm.blade.php
+++ resources/templates/site/userForm.blade.php
@@ -12,23 +12,12 @@
                     @endif
                     
                     <div class="row">
-                        <div class="col-sm-6">
+                        <div class="col-sm-12">
                             <div class="form-group fg-line">
                                 <label class="control-label" for="usernameField">Nombre de usuario</label>
                                 <input type="text" id="usernameField" name="username" class="form-control" value="{{ isset($this->user)?  $this->user->getUsername() : "" }}" autofocus="true">
                             </div>
                         </div>
-                        <div class="col-sm-6">
-                            <div class="form-group fg-line">
-                                <label class="control-label" for="typeField">Tipo</label>
-                                <select id="typeField" name="type" class="form-control">
-                                    <option value="1">Administrador</option>
-                                    <option value="2"{{ (isset($this->user) && $this->user->getType() == 2)? " selected=\"selected\"" : "" }}>Organizador</option>
-                                    <option value="2"{{ (isset($this->user) && $this->user->getType() == 3)? " selected=\"selected\"" : "" }}>Jugador</option>
-                                </select> 
-                            </div>
-                        </div>
-                        
                     </div>
                     
                     <div class="row">
@@ -47,6 +36,19 @@
                     </div>
                     
                     <div class="row">
+                        <div class="col-sm-12">
+                            <div class="form-group fg-line">
+                                <label class="control-label" for="typeField">Tipo</label>
+                                <select id="typeField" name="type" class="form-control">
+                                    <option value="1">Administrador</option>
+                                    <option value="2"{{ (isset($this->user) && $this->user->getType() == 2)? " selected=\"selected\"" : "" }}>Organizador</option>
+                                    <option value="2"{{ (isset($this->user) && $this->user->getType() == 3)? " selected=\"selected\"" : "" }}>Jugador</option>
+                                </select> 
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
                         <div class="col-sm-6">
                             <div class="form-group fg-line">
                                 <label class="control-label" for="firstnameField">Nombre</label>
@@ -60,6 +62,58 @@
                             </div>
                         </div>
                     </div>
+                    
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group fg-line">
+                                <label class="control-label" for="documentField">Nro Documento</label>
+                                <input type="number" id="documentField" name="documentNumber" class="form-control" value="{{ isset($this->user)?  $this->user->getDocumentNumber() : "" }}">
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group fg-line">
+                                <label class="control-label" for="birthDateField">Fecha de Nacimiento</label>
+                                <input type="date" id="birthDateField" name="birthDate" class="form-control" value="{{ isset($this->user)?  $this->user->getBirthDate() : "" }}">
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group fg-line">
+                                <label class="control-label" for="addressField">Dirección</label>
+                                <input type="text" id="addressField" name="address" class="form-control" value="{{ isset($this->user)?  $this->user->getAddress() : "" }}">
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group fg-line">
+                                <label class="control-label" for="emailField">Email</label>
+                                <input type="email" id="emailField" name="email" class="form-control" value="{{ isset($this->user)?  $this->user->getEmail() : "" }}">
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="form-group fg-line">
+                                <label class="control-label" for="contactVia1Field">Teléfono 1</label>
+                                <input type="text" id="contactVia1Field" name="contactVia1" class="form-control" value="{{ isset($this->user)?  $this->user->getContactVia1() : "" }}">
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="form-group fg-line">
+                                <label class="control-label" for="contactVia2Field">Teléfono 2</label>
+                                <input type="text" id="contactVia2Field" name="contactVia2" class="form-control" value="{{ isset($this->user)?  $this->user->getContactVia2() : "" }}">
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="form-group fg-line">
+                                <label class="control-label" for="contactVia3Field">Teléfono 3</label>
+                                <input type="text" id="contactVia3Field" name="contactVia3" class="form-control" value="{{ isset($this->user)?  $this->user->getContactVia3() : "" }}">
+                            </div>
+                        </div>
+                    </div>
+                    
                     <button type="submit" class="btn btn-primary">Guardar</button>
                 </form>
             </div>
--- sources/org/fmt/manager/UsersManager.php
+++ sources/org/fmt/manager/UsersManager.php
@@ -24,4 +24,14 @@ class UsersManager extends ModelManager {
     {
         return $this->createModelCollection(User::class, $this->getConnection()->createQuery("\"user\"")->addOrderBy("id")->get());
     }
+    
+    /**
+     * Obtiene un usuario a traves del id
+     * @param $id id del usuario
+     * @return User Usuario obtenido
+     */
+    public function getUser ($id)
+    {
+        return $this->createModel(User::class, $this->getConnection()->createQuery("\"user\"")->addWhere("id", "=", $id)->getFirst());
+    }
 }
