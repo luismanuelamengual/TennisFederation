c6c9b8aa8f772ae82893dc1b5e02a755f67583c4
Luis Manuel Amengual <luismanuelamengual@gmail.com>
2016-06-04 11:42:26 -0300

    Subo rutas cargadas desde el index

--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 
 require_once ("../../NeoPHP/autoload.php");
 $app = new NeoPHP\web\WebApplication(realpath(".."));
+$app->addRoute("/", "org\\fmt\\controller\\PublicSiteController");
+$app->addRoute("/dashboard/", "org\\fmt\\controller\\DashboardController");
+$app->addRoute("/session/", "org\\fmt\\controller\\SessionController");
+$app->addRoute("/category/", "org\\fmt\\controller\\CategoriesController");
+$app->addRoute("/club/", "org\\fmt\\controller\\ClubsController");
+$app->addRoute("/user/", "org\\fmt\\controller\\UsersController");
 $app->handleRequest();
-
-?>
