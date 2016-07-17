cb514aeaefccdaf319414a02ba2a54b18a068115
Luis Manuel Amengual <luismanuelamengual@gmail.com>
2016-07-16 23:29:21 -0300

    Agrego listado de torneos

--- sources/org/fmt/controller/TournamentsController.php
+++ sources/org/fmt/controller/TournamentsController.php
@@ -3,7 +3,9 @@
 namespace org\fmt\controller;
 
 use NeoPHP\web\http\RedirectResponse;
+use org\fmt\model\Club;
 use org\fmt\model\Tournament;
+use org\fmt\model\User;
 
 class TournamentsController extends SiteController 
 {   
@@ -21,25 +23,30 @@ class TournamentsController extends SiteController
     {
         $parameters = [];
         $parameters["tournament"] = !empty($id)? $this->findModel(Tournament::class, $id) : null;
-        $parameters["clubs"] = $this->findModels(\org\fmt\model\Club::class);
+        $parameters["clubs"] = $this->findModels(Club::class);
         return $this->createTemplateView("site.tournaments.form", $parameters);
     }
     
     public function createTournamentAction ()
     {
-        $this->insertModel($this->createModel(Tournament::class, $this->getRequest()->getParameters()->get()));
-        return new RedirectResponse($this->getUrl("club/showTournamentsList"));
+        $properties = $this->getRequest()->getParameters()->get();
+        $tournament = $this->createModel(Tournament::class, $properties);
+        $tournament->setClub(new Club($properties["clubid"]));
+        $tournament->setState(Tournament::STATE_INSCRIPTION);
+        $tournament->setOrganizer(new User($this->getSession()->userId));
+        $this->insertModel($tournament);
+        return new RedirectResponse($this->getUrl("tournament/showTournamentsList"));
     }
     
     public function updateTournamentAction ()
     {
         $this->updateModel($this->createModel(Tournament::class, $this->getRequest()->getParameters()->get()));
-        return new RedirectResponse($this->getUrl("club/showTournamentsList"));
+        return new RedirectResponse($this->getUrl("tournament/showTournamentsList"));
     }
     
     public function deleteTournamentAction ($id)
     {
         $this->removeModel(new Tournament($id));
-        return new RedirectResponse($this->getUrl("club/showTournamentsList"));
+        return new RedirectResponse($this->getUrl("tournament/showTournamentsList"));
     }
 }
